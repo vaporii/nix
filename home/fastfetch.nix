723c499adaac5f68e1ddb71d8488ade6e7df57e4
vaporii <phi548182@gmail.com>
2025-04-06 16:49:39 -0500

    fix fastfetch

--- home/fastfetch.nix
+++ home/fastfetch.nix
@@ -1,14 +1,18 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.fastfetch = {
+    package = (pkgs.fastfetch.overrideAttrs (finalAttrs: previousAttrs: {
+      cmakeFlags = [(lib.cmakeBool "ENABLE_IMAGEMAGICK7" true)];
+    }));
+    
     settings = {
       logo = {
-        type = "kitty-direct";
+        type = "kitty";
         source = "$(ls ${../assets/fastfetch}/*.png | shuf -n 1)";
 
-        width = 36;
-        height = 32;
+        width = 33;
+        height = 40;
 
         padding.left = 4;
         padding.right = 4;
@@ -17,6 +21,50 @@
       display = {
         separator = "";
       };
+
+            modules = [
+        { type = "custom"; format = "                    ハードウェア                    "; }
+        { type = "custom"; format = "┌──────────────────────────────────────────────────┐"; }
+
+        { type = "cpu"; key = " CPU "; }
+        { type = "gpu"; key = " GPU "; format = "{2} [{6}]"; }
+        { type = "memory"; key = " MEM "; }
+        "break"
+        {
+          type = "disk";
+          folders = "/nix:/persist";
+          key = " 󰋊 ";
+        }
+
+        { type = "custom"; format = "└──────────────────────────────────────────────────┘"; }
+        "break"
+
+        { type = "custom"; format = "                    ソフトウェア                    "; }
+        { type = "custom"; format = "┌──────────────────────────────────────────────────┐"; }
+
+        { type = "title"; key = " 󰁥 "; format = "{1}@{2}"; }
+        "break"
+        { type = "os"; key = "  "; }
+        { type = "kernel"; key = " 󰌽 "; format = "{1} {2}"; }
+        { type = "packages"; key = " 󰆧 "; }
+        "break"
+        { type = "terminal"; key = "  "; }
+        { type = "shell"; key = " 󱆃 "; }
+#        { type = "font"; key = " 󰬈 "; }
+        { type = "font"; key = " 󰬈 "; format = "Caskaydia Mono (8pt)"; }
+        "break"
+        { type = "wm"; key = "  "; }
+#        { type = "theme"; key = " 󰏘 "; }
+        { type = "theme"; key = " 󰏘 "; format = "gruvbox"; }
+        "break"
+        { type = "media"; key = " 󰝚 "; }
+        { type = "datetime"; key = " 󰃰 "; }
+
+        { type = "custom"; format = "└──────────────────────────────────────────────────┘"; }
+        "break"
+
+        "colors"
+      ];
     };
   };
 }
