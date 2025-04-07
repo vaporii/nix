5b0efb19ddce9a19750a3ca4b43a9079e59abb9f
vaporii <phi548182@gmail.com>
2025-04-07 09:56:24 -0500

    theme

--- home/programs/hyprland.nix
+++ home/programs/hyprland.nix
@@ -60,10 +60,13 @@
       gaps_in = 5;
       gaps_out = 10;
 
-      border_size = 1;
+      border_size = 2;
 
-      "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-      "col.inactive_border" = "rgba(595959aa)";
+      # "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      # "col.inactive_border" = "rgba(595959aa)";
+      
+      "col.active_border" = "rgba(a89984ff)";
+      "col.inactive_border" = "rgba(a899847e)";
 
       resize_on_border = true;
 
@@ -73,18 +76,18 @@
     };
 
     decoration = {
-      rounding = 10;
-      rounding_power = 2;
+      rounding = 0;
+      rounding_power = 0;
 
       active_opacity = 1.0;
       inactive_opacity = 0.9;
 
-      shadow = {
-        enabled = true;
-        range = 4;
-        render_power = 3;
-        color = "rgba(1a1a1aee)";
-      };
+      # shadow = {
+      #   enabled = true;
+      #   range = 4;
+      #   render_power = 3;
+      #   color = "rgba(1a1a1aee)";
+      # };
 
       blur = {
         enabled = true;
