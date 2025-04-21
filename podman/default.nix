a2283064bddd09c4bde450d82cc142a1940c0954
vaporii <phi548182@gmail.com>
2025-04-20 22:54:36 -0500

    podman

---
+++ podman/default.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, config, ... }:
+
+{
+  imports = [
+    ./traefik.nix
+    ./dashdot.nix
+  ];
+
+  options.podman.enable = lib.mkEnableOption "podman";
+
+  config = lib.mkIf config.podman.enable {
+    virtualisation.containers.enable = true;
+    virtualisation = {
+      podman = {
+        enable = true;
+        dockerCompat = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+      oci-containers.backend = "podman";
+    };
+
+    systemd.targets."podman-root" = {
+      unitConfig = {
+        Description = "podman root target";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+
+    systemd.services."podman-traefik-network" = {
+      path = [ pkgs.podman ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStop = "${pkgs.podman}/bin/podman network rm -f traefik_network";
+      };
+      script = ''
+        ${pkgs.podman}/bin/podman network inspect traefik_network || podman network create traefik_network
+      '';
+      partOf = [ "podman-root.target" ];
+      wantedBy = [ "podman-root.target" ];
+    };
+  };
+}
---
+++ podman/traefik.nix
@@ -0,0 +1,48 @@
+{ lib, config, ... }: {
+  options.traefik = {
+    enable = lib.mkEnableOption "traefik";
+    storage = lib.mkOption {
+      type = lib.types.oneOf [ lib.types.path lib.types.str ];
+      description = "string volume name or path to store traefik data";
+      default = "traefik-data";
+    };
+    acme = {
+      email = lib.mkOption {
+        type = lib.types.str;
+        description = "email for acme";
+        default = "phi548182@gmail.com";
+      };
+    };
+  };
+
+  config = lib.mkIf config.traefik.enable {
+    virtualisation.oci-containers.containers.traefik = {
+      image = "traefik";
+      volumes = [
+        "/var/run/podman/podman.sock:/var/run/docker.sock:ro"
+        "${config.traefik.storage}:/traefikconfig:rw"
+      ];
+      ports = [
+        "80:80/tcp"
+        "443:443/tcp"
+      ];
+      labels = {
+        "serverstransport.insecureskipverify" = "true";
+        "api.insecure" = "true";
+        "providers.docker" = "true";
+        "providers.docker.exposedbydefault" = "false";
+        "entrypoints.websecure.address" = ":443";
+        "entrypoints.websecure.transport.respondingTimeouts.readTimeout" = "10m";
+        "entrypoints.web.address" = ":80";
+        "entrypoints.web.http.redirections.entryPoint.to" = "websecure";
+        "entrypoints.web.http.redirections.entryPoint.scheme" = "https";
+        "entrypoints.web.http.redirections.entryPoint.permanent" = "true";
+        "certificatesresolvers.myresolver.acme.tlschallenge" = "true";
+        "providers.file.directory" = "/traefikconfig/config";
+        "providers.file.watch" = "true";
+        "certificatesresolvers.myresolver.acme.email" = config.traefik.acme.email;
+        "certificatesresolvers.myresolver.acme.storage" = "/traefikconfig/le/acme.json";
+      };
+    };
+  };
+}
