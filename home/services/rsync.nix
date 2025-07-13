c9497272db1480d2f595f46d95c974256896a2cf
vaporii <phi548182@gmail.com>
2025-07-13 00:43:30 -0500

    rsync

--- home/services/rsync.nix
+++ home/services/rsync.nix
@@ -17,9 +17,35 @@
           ${pkgs.inotify-tools}/bin/inotifywait -m -e create -e delete -r ~/Persist |
           while read path action file; do
             echo "The file '$file' at '$path' was $action"
-            ${pkgs.rsync}/bin/rsync -avz --protocol=31 -e "${pkgs.openssh}/bin/ssh -i ~/.ssh/v8p_ed25519" ~/Persist vaporii@vaporii.net:~/back
+            ${pkgs.rsync}/bin/rsync -avzu --protocol=31 -e "${pkgs.openssh}/bin/ssh -p 25567" ~/Persist vaporii@vaporii.net:~/back
           done
         ''}";
+      };
+    };
+
+    systemd.user.services.rsync-pull = {
+      Unit = {
+        Description = "pull Persist from server";
+      };
+      Service = {
+        ExecStart = "${pkgs.writeShellScript "pull-files" ''
+          #!/run/current-system/sw/bin/bash
+          ${pkgs.rsync}/bin/rsync -avzu --protocol=31 -e "${pkgs.openssh}/bin/ssh -p 25567" vaporii@vaporii.net:~/back/Persist/ ~/Persist
+        ''}";
+      };
+    };
+
+    systemd.user.timers.rsync-pull-timer = {
+      Unit = {
+        Description = "Periodic pull from server";
+      };
+      Timer = {
+        OnBootSec = "5min";
+        OnUnitActiveSec = "1h";
+        Persistent = true;
+      };
+      Install = {
+        WantedBy = [ "timers.target" ];
       };
     };
   };
