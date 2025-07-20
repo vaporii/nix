799272c96d1dcf33c817219d0f786a3e9852aac5
vaporii <phi548182@gmail.com>
2025-07-19 19:21:43 -0500

    add puredata

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -148,6 +148,7 @@
       eclipses.eclipse-java
       qemu_kvm
       wootility
+      puredata
     ];
     hashedPassword = "$y$j9T$h14SkfRLxr/uUwoJbEb35.$l9k5T4/xHp4h1V95l/OdaYjC8Sb4AFXpvkPaqYJKE97";
   };
